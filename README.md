069fb1ff2185a4dd6407399a44d68b5e30b5afde
Aleksey Komissarov <ad3002@gmail.com>
2017-12-17 16:06:11 +0300

    iter over read

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ make
 ```
 
 ```
+
+mkdir bin
+
 g++ Compute_index.cpp  -std=c++11 -pthread -O3 debrujin.hpp debrujin.cpp read.hpp read.cpp kmers.hpp kmers.cpp settings.cpp settings.hpp hash.hpp hash.cpp && mv a.out bin/compute_index.exe
 
 g++ Compute_aindex.cpp  -std=c++11 -pthread -O3 debrujin.hpp debrujin.cpp read.hpp read.cpp kmers.hpp kmers.cpp settings.cpp settings.hpp hash.hpp hash.cpp && mv a.out bin/compute_aindex.exe
--- tests/genes.py
+++ tests/genes.py
@@ -4,11 +4,35 @@ import sys
 sys.path.append("/mnt/guatemala/akomissarov/Boechera_spatifolia/aindex")
 
 from aindex import *
+from trseeker.seqio.fasta_file import sc_iter_fasta
 
-settings = {
-  "index_prefix": "/mnt/guatemala/akomissarov/Boechera_spatifolia/raw.23.L3",
-  "aindex_prefix": "/mnt/guatemala/akomissarov/Boechera_spatifolia/raw.23.L3",
-  "reads_file": "/mnt/guatemala/akomissarov/Boechera_spatifolia/raw.reads",
-}
 
-index = load_aindex(settings)
+if __name__ == '__main__':
+    
+
+    settings = {
+      "index_prefix": "/mnt/guatemala/akomissarov/Boechera_spatifolia/raw.23.L3",
+      "aindex_prefix": "/mnt/guatemala/akomissarov/Boechera_spatifolia/raw.23.L3",
+      "reads_file": "/mnt/guatemala/akomissarov/Boechera_spatifolia/raw.reads",
+      "gene_fasta": "/mnt/guatemala/akomissarov/Boechera_spatifolia/apr1.fa",
+    }
+
+    k = 23
+    index = load_aindex(settings)
+
+    for seq_obj in sc_iter_fasta(settings["gene_fasta"]):
+
+        for i in xrange(seq_obj.length-k+1):
+            kmer = seq_obj.sequence[i:i+k]
+
+            print i, kmer, index[kmer]
+
+            for read in iter_reads_by_kmer(kmer, index):
+                print read
+
+            raw_input("?")
+
+
+
+
+
