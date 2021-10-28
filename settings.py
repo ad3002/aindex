b6df241218775073c98bb36fce0cbc40fd4017bd
Aleksey Komissarov <ad3002@gmail.com>
2021-10-28 12:56:19 +0000

    feat/add local testing paths

--- settings.py
+++ settings.py
@@ -6,6 +6,10 @@
 #@contact: ad3002@gmail.com
 
 dll_paths = [
-	'python_wrapper.so',
-	'python_wrapper.dylib',
+    # 'python_wrapper.so',
+    # 'python_wrapper.dylib',
+    '/mnt/voronezh/pipeline_ak/aindex/python_wrapper.so',
+    '/home/akomissarov/Dropbox/PyBioSnippets/aindex/python_wrapper.so',
+    '/Users/akomissarov/Dropbox/PyBioSnippets/aindex/python_wrapper.so',
+    '/home/akomissarov/Dropbox/workspace/PyBioSnippets/aindex/python_wrapper.so',
 ]
