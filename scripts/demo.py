3e96cef5abbed415b64f140d6a1f12a69d3f560b
Aleksey Komissarov <ad3002@gmail.com>
2024-05-30 16:50:48 +0300

    fix demo

--- scripts/demo.py
+++ scripts/demo.py
@@ -8,13 +8,13 @@
 from aindex import *
 
 settings = {
-  "index_prefix": "tests/kmers.23",
-  "aindex_prefix": "tests/kmers.23",
-  "reads_file": "tests/reads.reads",
+  "index_prefix": "tests/test.23",
+  "aindex_prefix": "tests/test.23",
+  "reads_file": "tests/test.reads",
+  "sdat_file": "tests/test.23.sdat",
 }
 
 index = load_aindex(settings)
-
 
 k = 23
 sequence = "TAAGTTATTATTTAGTTAATACTTTTAACAATATTATTAAGGTATTTAAAAAATACTATTATAGTATTTAACATAGTTAAATACCTTCCTTAATACTGTTAAATTATATTCAATCAATACATATATAATATTATTAAAATACTTGATAAGTATTATTTAGATATTAGACAAATACTAATTTTATATTGCTTTAATACTTAATAAATACTACTTATGTATTAAGTAAATATTACTGTAATACTAATAACAATATTATTACAATATGCTAGAATAATATTGCTAGTATCAATAATTACTAATATAGTATTAGGAAAATACCATAATAATATTTCTACATAATACTAAGTTAATACTATGTGTAGAATAATAAATAATCAGATTAAAAAAATTTTATTTATCTGAAACATATTTAATCAATTGAACTGATTATTTTCAGCAGTAATAATTACATATGTACATAGTACATATGTAAAATATCATTAATTTCTGTTATATATAATAGTATCTATTTTAGAGAGTATTAATTATTACTATAATTAAGCATTTATGCTTAATTATAAGCTTTTTATGAACAAAATTATAGACATTTTAGTTCTTATAATAAATAATAGATATTAAAGAAAATAAAAAAATAGAAATAAATATCATAACCCTTGATAACCCAGAAATTAATACTTAATCAAAAATGAAAATATTAATTAATAAAAGTGAATTGAATAAAATTTTGAAAAAAATGAATAACGTTATTATTTCCAATAACAAAATAAAACCACATCATTCATATTTTTTAATAGAGGCAAAAGAAAAAGAAATAAACTTTTATGCTAACAATGAATACTTTTCTGTCAAATGTAATTTAAATAAAAATATTGATATTCTTGAACAAGGCTCCTTAATTGTTAAAGGAAAAATTTTTAACGATCTTATTAATGGCATAAAAGAAGAGATTATTACTATTCAAGAAAAAGATCAAACACTTTTGGTTAAAACAAAAAAAACAAGTATTAATTTAAACACAATTAATGTGAATGAATTTCCAAGAATAAGGTTTAATGAAAAAAACGATTTAAGTGAATTTAATCAATTCAAAATAAATTATTCACTTTTAGTAAAAGGCATTAAAAAAATTTTTCACTCAGTTTCAAATAATCGTGAAATATCTTCTAAATTTAATGGAGTAAATTTCAATGGATCCAATGGAAAAGAAATATTTTTAGAAGCTTCTGACACTTATAAACTATCTGTTTTTGAGATAAAGCAAGAAACAGAACCATTTGATTTCATTTTGGAGAGTAATTTACTTAGTTTCATTAATTCTTTTAATCCTGAAGAAGATAAATCTATTGTTTTTTATTACAGAAAAGATAATAAAGATAGCTTTAGTACAGAAATGTTGATTTCAATGGATAACTTTATGATTAGTTACACATCGGTTAATGAAAAATTTCCAGAGGTAAACTACTTTTTTGAATTTGAACCTGAAACTAAAATAGTTGTTCAAAAAAATGAATTAAAAGATGCACTTCAAAGAATTCAAACTTTGGCTCAAAATGAAAGAACTTTTTTATGCGATATGCAAATTAACAGTTCTGAATTAAAAATAAGAGCTATTGTTAATAATATCGGAAATTCTCTTGAGGAAATTTCTTGTCTTAAATTTGAAGGTTATAAACTTAATATTTCTTTTAACCCAAGTTCTCTATTAGATCACATAGAGTCTTTTGAATCAAATGAAATAAATTTTGATTTCCAAGGAAATAGTAAGTATTTTTTGATAACCTCTAAAAGTGAACCTGAACTTAAGCAAATATTGGTTCCTTCAAGATAATGAATCTTTACGATCTTTTAGAACTACCAACTACAGCATCAATAAAAGAAATAAAAATTGCTTATAAAAGATTAGCAAAGCGTTATCACCCTGATGTAAATAAATTAGGTTCGCAAACTTTTGTTGAAATTAATAATGCTTATTCAATATTAAGTGATCCTAACCAAAAGGAAAAATATGATTCAATGCTGAAAGTTAATGATTTTCAAAATCGCATCAAAAATTTAGATATTAGTGTTAGATGACATGAAAATTTCATGGAAGAACTCGAACTTCGTAAGAACTGAGAATTTGATTTTTTTTCATCTGATGAAGATTTCTTTTATTCTCCATTTACAAAAA"
@@ -34,18 +34,34 @@ for i, read in enumerate(index.iter_reads()):
         break
     print(i, read)
 
-print("Task 3. Iter reads by kmer, returs (start, next_read_start, read, pos_if_uniq|None, all_poses)")
+print("Task 3. Iter overall reads by kmer, check the consistency")
+with open(settings["sdat_file"]) as fh:
+    for i, line in enumerate(fh):
+        kmer, tf = line.strip().split("\t")
+        tf = int(tf)
+        print(kmer, tf, index[kmer])
+        if i == 20:
+            break
+
+with open(settings["sdat_file"]) as fh:
+    for line in fh:
+        kmer, tf = line.strip().split("\t")
+        tf = int(tf)
+        # print(kmer, tf, index[kmer])
+        assert tf == index[kmer]
+
+print("Task 4. Iter reads by kmer, returs (start, next_read_start, read, pos_if_uniq|None, all_poses)")
 # raw_input("\nReady?")
 for read in iter_reads_by_kmer(test_kmer, index):
     print(read)
 
 
-print("Task 4. Get distances in reads for two kmers, returns a list of (rid, left_kmer_pos, right_kmer_pos) tuples.")
+print("Task 5. Get distances in reads for two kmers, returns a list of (rid, left_kmer_pos, right_kmer_pos) tuples.")
 # raw_input("\nReady?")
 print(get_left_right_distances(test_kmer, right_kmer, index))
 
 
-print("Task 5. Get layout for kmer, returns (max_pos, reads, lefts, rights, rids, starts), for details see source code")
+print("Task 6. Get layout for kmer, returns (max_pos, reads, lefts, rights, rids, starts), for details see source code")
 # raw_input("\nReady?")
 max_pos, reads, lefts, rights, rids, starts = get_layout_for_kmer(right_kmer, index)
 print("Central layout:")
@@ -56,14 +72,14 @@ print(lefts)
 print("Right flanks:")
 print(rights)
 
-print("Task 6. Iter reads by sequence, returтs (start, next_read_start, read, pos_if_uniq|None, all_poses)")
+print("Task 7. Iter reads by sequence, returтs (start, next_read_start, read, pos_if_uniq|None, all_poses)")
 # raw_input("\nReady?")
 sequence = "AATATTATTAAGGTATTTAAAAAATACTATTATAGTATTTAACATA"
 for read in iter_reads_by_sequence(sequence, index):
     print(read)
 
 
-print("Task 7. Iter reads by kmer with reads as SE, returns (start, next_read_start, subread, kmere_pos, -1|0|1 for spring_pos, was_reversed, poses_in_read)")
+print("Task 8. Iter reads by kmer with reads as SE, returns (start, next_read_start, subread, kmere_pos, -1|0|1 for spring_pos, was_reversed, poses_in_read)")
 # raw_input("\nReady?")
 user_reads = set()
 sequence = "AATATTATTAAGGTATTTAAAAAATACTATTATAGTATTTAACATA"
