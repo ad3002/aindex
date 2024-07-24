9d7e4e43ced517ef81e6757ff9a0d7a9c48f78c6
Aleksey Komissarov <ad3002@gmail.com>
2024-07-24 22:11:34 +0400

    next try

--- setup.py
+++ setup.py
@@ -1,35 +1,27 @@
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext as build_ext_orig
+from setuptools.command.install import install
 import subprocess
 import os
-import shutil
 import glob
-from setuptools.command.install import install
-
+import shutil
 
 class build_ext(build_ext_orig):
     def run(self):
-        # Run makefile commands to build C++ components
         subprocess.check_call(['make', 'clean'])
         subprocess.check_call(['make', 'all'])
-        
-        # Copy the pre-built shared object to the correct location
         build_lib = self.build_lib
         package_dir = os.path.join(build_lib, 'aindex', 'core')
         os.makedirs(package_dir, exist_ok=True)
-        
-        # Find the .so file and rename it
-        so_file = glob.glob(os.path.join('aindex', 'core', 'python_wrapper*.so'))[0]
-        shutil.copy(so_file, os.path.join(package_dir, 'python_wrapper.so'))
+        so_files = glob.glob(os.path.join('aindex', 'core', 'python_wrapper*.so'))
+        if so_files:
+            shutil.copy(so_files[0], os.path.join(package_dir, 'python_wrapper.so'))
 
 class CustomInstall(install):
     def run(self):
         install.run(self)
-        # Ensure bin directory exists in the installation
         bin_dir = os.path.join(self.install_scripts, 'bin')
         os.makedirs(bin_dir, exist_ok=True)
-        
-        # Copy bin files
         for file in glob.glob('bin/*'):
             shutil.copy(file, bin_dir)
 
@@ -52,6 +44,7 @@ setup(
     include_package_data=True,
     package_data={
         'aindex.core': ['*.so'],
+        '': ['bin/*'],
     },
     data_files=[
         ('bin', glob.glob('bin/*')),
