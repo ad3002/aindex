f61d9f7edc0fe2b9322733c9c0feb58528c9c92a
Aleksey Komissarov <ad3002@gmail.com>
2024-07-25 17:05:45 +0400

    replace python with python3

--- scripts/compute_index.py
+++ scripts/compute_index.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # @created: 01.01.2017
--- scripts/reads_to_fasta.py
+++ scripts/reads_to_fasta.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # @created: 10.10.2013
